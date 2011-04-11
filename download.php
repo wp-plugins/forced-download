120a2e2879fa7287b7de0413736a1c8e8f81df3a
areimann <areimann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-11 23:13:20 +0000

    Fixing some wording
    git-svn-id: https://plugins.svn.wordpress.org/forced-download/tags/1.0.1@371775 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ download.php
@@ -0,0 +1,74 @@
+<?php
+// this will give us access to all of the WP functions
+if (file_exists('../../../wp-blog-header.php')) {
+	require_once('../../../wp-blog-header.php');
+} else {
+	echo 'Unable to locate the CMS.  "Forced Download" can not function without the CMS libraries.';
+	exit();
+}
+
+// get everything past wp-content (including wp-content)
+// had to pass the variable via URL because the upload dir can always change
+$path = $_GET['path'];
+$path = preg_replace('/^.*wp-content/', '', $path);
+
+$filename = $_GET['file'];
+
+$location = WP_CONTENT_DIR . $path . $filename;
+
+// making the file exists, if not, break
+if (!file_exists($location)) {
+	echo 'File Not Found.';
+} else {
+	$length = filesize($location);
+	$file_ext = strtolower(substr(strrchr($filename,"."),1));
+
+	// thanks aaron dugan for this (aarondugan.com)
+	//This will set the Content-Type to the appropriate setting for the file
+	switch($file_ext) {
+		case "pdf": $file_type = "application/pdf"; break;
+		case "exe": $file_type = "application/octet-stream"; break;
+		case "zip": $file_type = "application/zip"; break;
+		case "doc": $file_type = "application/msword"; break;
+		case "xls": $file_type = "application/vnd.ms-excel"; break;
+		case "ppt": $file_type = "application/vnd.ms-powerpoint"; break;
+		case "gif": $file_type = "application/gif"; break;
+		case "png": $file_type = "application/png"; break;
+		case "jpeg": $file_type = "application/jpg"; break;
+		case "jpg": $file_type = "application/jpg"; break;
+		case "mp3": $file_type = "application/mpeg"; break;
+		case "wav": $file_type = "application/x-wav"; break;
+		case "mpeg": $file_type = "application/mpeg"; break;
+		case "mpg": $file_type = "application/mpeg"; break;
+		case "mpe": $file_type = "application/mpeg"; break;
+		case "mov": $file_type = "application/quicktime"; break;
+		case "avi": $file_type = "application/x-msvideo"; break;
+
+	  // don't allow these
+		case "php":
+		case "htm":
+		case "html":
+		case "txt": die("<b>Cannot be used for ". $file_ext ." files!</b>"); break;
+
+		default: $file_type = "application/force-download";
+	}
+
+	//Begin writing headers
+	header("Pragma: public");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+	header("Cache-Control: public");
+	header("Content-Description: File Transfer");
+	header("Content-Type: $file_type");
+
+	//Force the download
+	$header="Content-Disposition: attachment; filename=".$filename.";";
+	header($header);
+	header("Content-Transfer-Encoding: binary");
+	header("Content-Length: ".$length);
+
+	// pulling the file and shooting it to the browser
+	readfile($location);
+}
+
+?>
---
+++ trunk/index.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Forced Download
+Plugin URI: http://www.sideways8.com/plugins/forced-download
+Description: This forces a download (vs. show up in your browser) for any "a href" that has the class "forced-download".  Works for jpg, pdf, doc, mp3, etc.
+Version: 1.0.1
+Author: Aaron Reimann
+Author URI: http://www.aaronreimann.com
+License: GPL3
+
+Copyright 2011 Aaron Reimann aaronr@sideways8.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 3, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action('wp_print_scripts','forced_download_location');
+
+// this function is strictly to pass the location of the plugin so javascript will know where download.php
+// therefore javascript will be able to redirect URLS's to download.php
+function forced_download_location(){
+	if (!is_admin()) {
+		echo '<script type="text/javascript">var fdl =  "'. WP_PLUGIN_URL .'/forced-download/";</script>';
+	}
+}
+
+function init_fd() {
+	if (!is_admin()) {
+		wp_enqueue_script('jquery.url', WP_PLUGIN_URL . '/forced-download/js/jquery.url.js', array('jquery'));
+		wp_enqueue_script( 'forced-download', WP_PLUGIN_URL . '/forced-download/js/forced-download.js', array('jquery'));
+	}
+}
+add_action('init', 'init_fd');
+
+?>
